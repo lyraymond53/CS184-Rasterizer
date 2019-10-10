426e6de7eceef0f5727c5afdd4b1f0e8d6f211ae
Ray <lyraymond53@gmail.com>
2019-10-09 17:53:25 -0700

    Reclone repo from classroom acc

---
+++ cmake-build-debug/src/CMakeFiles/draw.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/drawrend.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/drawrend.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/main.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/main.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/svg.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/svg.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/svgparser.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/svgparser.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/texture.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/texture.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/transforms.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/transforms.cpp.o"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src/triangulation.cpp" "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/src/CMakeFiles/draw.dir/triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "/Library/Frameworks/Mono.framework/Headers"
+  "/usr/local/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/cmake_install.cmake
@@ -0,0 +1,52 @@
+# Install script for directory: /Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/draw")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53" TYPE EXECUTABLE FILES "/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/cmake-build-debug/draw")
+  if(EXISTS "$ENV{DESTDIR}/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/draw" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/draw")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}/Users/raymond/Documents/CS184/p1-rasterizer-lyraymond53/draw")
+    endif()
+  endif()
+endif()
+
